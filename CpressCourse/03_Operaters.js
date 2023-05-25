70df83877ec95261b5e506e1538d76ba6be377d0
Cihan Aslan <cihan.aslan.qa@gmail.com>
2023-05-25 23:32:29 +0300

    operators

--- CpressCourse/03_Operaters.js
+++ CpressCourse/03_Operaters.js
@@ -1,8 +1,8 @@
 // Order of operators in JS is same with JAVA
-console.log(4+4*(-1)-8/2);
+console.log(4 + 4 * -1 - 8 / 2);
 // assignment operator
 let x = 5;
-x+=5;
+x += 5;
 console.log(x); //10
 
 // comparison: similar to Java. But there is some diff points
@@ -10,14 +10,15 @@ console.log(x); //10
 let one = 1;
 let one2 = 1;
 
-let one_str = '1';
-let one_str2 = '2';
+let one_str = "1";
+let one_str2 = "2";
 
 console.log(one == one2); //looking value
 console.log(one_str == one_str2);
 console.log(one == one_str); //looking value
 console.log(one === one_str); //looking value and data type together
 
-
-
 //logical operators same like Java. We have only short circuit &&(and), ||(or) operators
+//control flow statements same as Java. if-else/switch/break/continue
+//ternary operators same as Java
+//Loops (while and for) same as Java
